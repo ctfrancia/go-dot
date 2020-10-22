29122b663bffc4c5e06b643029669e2ddac7edc4
Christian Francia <christian.francia@adamo.es>
2020-10-22 16:44:40 +0200

    add cobra-cli

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ctfrancia/go-dot
 
 go 1.15
+
+require github.com/spf13/cobra v1.1.1 // indirect
--- main.go
+++ main.go
@@ -1,10 +1,63 @@
 package main
 
 import (
-	"fmt"
-	"github.com/ctfrancia/go-dot/morestrings"
+	/*
+		"fmt"
+		// "github.com/google/go-github/v32/github"
+		// "flag"
+		"bufio"
+		// "io/ioutil"
+		"log"
+		"os"
+		"os/user"
+		"path/filepath"
+		"strings"
+	*/
+	"github.com/ctfrancia/go-dot/pkg/pathmanager"
 )
 
+type dotFiles struct {
+	zshrcPath string
+}
+
 func main() {
-	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
+	// var df dotFiles
+	// var zshrcPath string
+	// var initVimPath string
+	pathmanager.StartUp()
+
+	/*
+		usr, err := user.Current()
+		check(err)
+
+		godotFile := filepath.Join(usr.HomeDir, "/godot")
+		check(err)
+
+		if _, err := os.Stat(godotFile); os.IsNotExist(err) {
+			err = os.MkdirAll(godotFile, 0777)
+			check(err)
+
+			f, err := os.Create(filepath.Join(godotFile, "config.json"))
+			check(err)
+			defer f.Close()
+		}
+	*/
+
+	/*
+		r := bufio.NewReader(os.Stdin)
+
+		fmt.Print("do you wish to track your zshrc file(y/n)?: ")
+		text, _ := r.ReadString('\n')
+		text = strings.TrimSuffix(text, "\n")
+
+		if text == "y" || text == "yes" {
+			fmt.Print("is it located in the default location(y/n)?: ")
+			text, _ = r.ReadString('\n')
+			text = strings.TrimSuffix(text, "\n")
+			if text == "y" || text == "yes" {
+				df.zshrcPath = filepath.Join(usr.HomeDir, ".zshrc")
+				fmt.Println(df.zshrcPath)
+			}
+		}
+	*/
 }
