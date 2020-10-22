650d757869212313718ca8e12029875df645c8f4
Christian Francia <christian.francia@adamo.es>
2020-10-22 10:09:55 +0200

    add folder structure and packages

--- pkg/pathmanager/pathmanager.go
+++ pkg/pathmanager/pathmanager.go
@@ -2,17 +2,68 @@
 package pathmanager
 
 import (
-	"fmt"
+	// "fmt"
+	"log"
+	"os"
+	"os/user"
+	"path/filepath"
 )
 
 // userpaths defines how the paths look for each valid fiel
-type userpaths struct {
+type Userpaths struct {
 	zshPath  string
 	nvimPath string
 }
 
+func check(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// New returns a new instance of userpaths to access it's methods
+func New() *Userpaths {
+	var up Userpaths
+	return &up
+}
+
 // StartUp is this initial function that will check the user to see if the user is new or the root directory is already there
 // if the user is new then it will start the prompt. If not then it will prepare the files
-func StartUp() {
-	fmt.Println("asdf")
+func StartUp() *Userpaths {
+	var filepaths Userpaths
+
+	exists := goDotFolderExists()
+	if exists {
+		return &filepaths
+	}
+
+	return &filepaths
+}
+
+func pathValidator(p, filename string) error {
+	return nil
+}
+
+func goDotFolderExists() bool {
+	usr, err := user.Current()
+	check(err)
+
+	godotFile := filepath.Join(usr.HomeDir, "/godot")
+	check(err)
+
+	if _, err := os.Stat(godotFile); os.IsNotExist(err) {
+		err = os.MkdirAll(godotFile, 0777)
+		check(err)
+
+		f, err := os.Create(filepath.Join(godotFile, "config.json"))
+		check(err)
+		defer f.Close()
+	}
+
+	return true
+}
+
+func (*Userpaths) AddPath(p string) error {
+
+	return nil
 }
---
+++ pkg/prompts/prompts.go
@@ -0,0 +1,48 @@
+package prompts
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/ctfrancia/go-dot/pkg/pathmanager"
+	"os"
+	// "path/filepath"
+	"strings"
+)
+
+var filesToTrack []string = []string{"zsh", "init.vim"}
+
+// StartQuestions function will initialize the CLI questions to fetch the paths from os.Stdin
+func StartQuestions() {
+	pm := pathmanager.New()
+	fmt.Println(pm)
+	r := bufio.NewReader(os.Stdin)
+
+	/*
+		for i, f := range filesToTrack {
+		}
+	*/
+
+	// TODO this questionnaire needs to be moved into the loop above
+	fmt.Print("do you wish to track your zshrc file(y/n)?: ")
+	text, _ := r.ReadString('\n')
+	text = strings.TrimSuffix(text, "\n")
+
+	if text == "y" || text == "yes" {
+		fmt.Print("is it located in the default location(y/n)?: ")
+		text, _ = r.ReadString('\n')
+		text = strings.TrimSuffix(text, "\n")
+		if text == "y" || text == "yes" {
+			/*
+				df.zshrcPath = filepath.Join(usr.HomeDir, ".zshrc")
+				fmt.Println(df.zshrcPath)
+			*/
+		}
+	}
+}
+
+/*
+func defaultPathLocation(pm *Userpaths) {
+	pm.zshrcPath = filepath.Join(usr.HomeDir, ".zshrc")
+	fmt.Println(pm.zshrcPath)
+}
+*/
